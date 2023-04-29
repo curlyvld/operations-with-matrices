1b1c912e169cd312129dde5945cd8005d880ccb6
Vladislav <124431226+curlyvld@users.noreply.github.com>
2023-04-29 15:40:36 +0400

    Update lab2dm.js

--- lab2dm.js
+++ lab2dm.js
@@ -35,22 +35,22 @@ function validate (input){
 function count(){
   let arr=document.getElementById("1");
   if (validate(arr.value)){
-      let a=arr.value.split("\n");
+      let a=arr.value.split("\n"); //Строка матрицы arr.value разбивается по символу новой строки ("\n") и сохраняется в переменной a в виде массива строк.
       let mnogitel;
       let reflexivity=true;
       let simmetry=true;
       let skewsymmetry=true;
       let transitivity=true;
       for (let i=0;i<a.length;i++){
-      for(let j=0;j<a.length;j++){
+      for(let j=0;j<a.length;j++){ //Происходит двойной цикл for для проверки симметричности матрицы. Если элементы матрицы a[i][j] и a[j][i] не равны, то переменная symmetry устанавливается в false.
           if(a[i][j]!=a[j][i]){
               simmetry=false;
           }//симметрична относительньно главной диагонали
         }
       }
       // Проходим по всем элементам матрицы
-     for (let i = 0; i < a.length; i++) {
-        for (let j = i + 1; j < a[i].length; j++) {
+      for (let i = 0; i < a.length; i++) {
+        for (let j = i + 1; j < a[i].length; j++) { //Двойной цикл for для проверки кососимметричности матрицы. Если элементы матрицы a[i][j] и a[j][i] не равны, то переменная skewSymmetry устанавливается в false.
           if (a[i][j] !== a[j][i]) {
             skewsymmetry = false;
             
@@ -58,7 +58,7 @@ function count(){
         }
       }
       for (let i=0;i<a.length;i++){
-        if(a[i][i]=="0"){
+        if(a[i][i]=="0"){ //Цикл for для проверки рефлексивности матрицы. Если элементы на главной диагонали матрицы a[i][i] равны "0", то переменная reflexivity устанавливается в false
             reflexivity=false;
         }//единичная главная ось
       }
@@ -68,7 +68,7 @@ function count(){
         for (let j = 0; j < a[i].length; j++) {
           // Если элементы i и j связаны, то ищем элементы, связанные с j
           if (a[i][j] === 1) {
-            for (let k = 0; k < matrix[j].length; k++) {
+            for (let k = 0; k < matrix[j].length; k++) { //Двойной цикл for используется для проверки транзитивности матрицы. Если элементы a[i][j] и a[j][k] равны 1, то элемент a[i][k] устанавливается в 1, и переменная transitivity устанавливается в false.
               // Если элементы j и k связаны, то делаем связь между i и k
               if (a[j][k] === 1) {
                 a[i][k] = 1;
@@ -80,28 +80,28 @@ function count(){
       }
 
     if (simmetry == true){
-        document.getElementById("simmetry").innerHTML="симметрична"
+        document.getElementById("simmetry").innerHTML="Cимметричность: симметрична"
     }
     else{
-        document.getElementById("simmetry").innerHTML="не симметрична"
+        document.getElementById("simmetry").innerHTML="Симметричность: не симметрична"
     }
     if (skewsymmetry == true){
-        document.getElementById("skewsymmetry").innerHTML="кососимметрична"
+        document.getElementById("skewsymmetry").innerHTML="Кососимметричность: кососимметрична"
     }
     else{
-        document.getElementById("skewsymmetry").innerHTML="не кососимметрична"
+        document.getElementById("skewsymmetry").innerHTML="Кососимметричность: не кососимметрична"
     }
     if (transitivity == true){
-        document.getElementById("transitivity").innerHTML="транзитивна"
+        document.getElementById("transitivity").innerHTML="Транзитивность: транзитивна"
     }
     else{
-        document.getElementById("transitivity").innerHTML="не транзитивна"
+        document.getElementById("transitivity").innerHTML="Транзитивность: не транзитивна"
     }
     if (reflexivity == true){
-        document.getElementById("reflexivity").innerHTML="рефлексивна"
+        document.getElementById("reflexivity").innerHTML="Рефлексивность: рефлексивна"
     }
     else{
-        document.getElementById("reflexivity").innerHTML="не рефлексивна"
+        document.getElementById("reflexivity").innerHTML="Рефлексивность: не рефлексивна"
    }
 } 
 else {
